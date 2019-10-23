d0f1166db50c2317f0f58169a7412171bb462a98
dribaJL <samvid.jhaveri@bhge.com>
2019-10-23 15:50:07 -0500

    This is first commit and final as well.

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed;
+    public float jump;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        if(Input.GetAxis("Jump") > 0)
+        {
+            rb.AddForce(Vector2.up * jump, ForceMode2D.Impulse);
+        }
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
+    }
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+    public GameObject[] platforms;
+    public float yDistance;
+    public float xDistance;
+
+    public float minXDistance;
+    public float maxXDistance;
+    public GameObject player;
+    public GameObject lastGenerated;
+
+    public GameObject paper;
+    public GameObject lastPaperObject; 
+
+    private List<GameObject> oldPlatforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldPlatforms = new List<GameObject>();
+        InvokeRepeating("Generation", 3, 3);
+        InvokeRepeating("CleanUp", 15, 10);
+        InvokeRepeating("PaperGeneration", 6, 15);
+
+    }
+
+    // Update is called once per frame
+
+     
+    void Generation()
+    {
+
+            int i = Random.Range(0, platforms.Length);
+            float newXPos = lastGenerated.transform.position.x + Random.Range(-xDistance, xDistance);
+            float clampedXPos = Mathf.Clamp(newXPos, minXDistance, maxXDistance);
+            float newYPos = lastGenerated.transform.position.y + Random.Range(1.5f, yDistance);
+            Vector2 newPosition = new Vector2(clampedXPos, newYPos);
+
+            GameObject newPlatform = Instantiate(platforms[i], newPosition, Quaternion.identity);
+            oldPlatforms.Add(lastGenerated);
+            lastGenerated = newPlatform;
+
+    }
+
+    void CleanUp()
+    {
+        foreach(GameObject uselessPlatform in oldPlatforms)
+        {
+            Destroy(uselessPlatform);
+        }
+        oldPlatforms.Clear();
+    }
+
+    void PaperGeneration()
+    {
+        Vector2 newPaperObjectPosition = lastPaperObject.transform.position + new Vector3(0, 15.17f,0);
+        GameObject newPaperObject = Instantiate(paper, newPaperObjectPosition, Quaternion.identity);
+        lastPaperObject = newPaperObject;
+
+    }
+
+}
